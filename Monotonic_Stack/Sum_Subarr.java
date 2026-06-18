282aad2e9bed7c759475fbeebce0b7f17b2ea3d8
SathwikaDasari <sathwikareddy059@gmail.com>
2026-06-18 22:01:10 +0530

    Added Sum of Subarray

--- Monotonic_Stack/Sum_Subarr.java
+++ Monotonic_Stack/Sum_Subarr.java
@@ -1,3 +1,4 @@
+class Solution {
     public long subArrayRanges(int[] A) {
         long res = 0;
         for (int i = 0; i < A.length; i++) {
@@ -5,8 +6,9 @@
             for (int j = i; j < A.length; j++) {
                 max = Math.max(max, A[j]);
                 min = Math.min(min, A[j]);
-                res += max - min;
+                res += (long)(max - min);
             }
         }
         return res;
     }
+}
